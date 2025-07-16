b2d0d965b39b05132f2eb838ec9af7793c883292
Misha <mishaelb7@gmail.com>
2025-07-16 13:19:32 +0300

    pagination

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,26 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useRef } from "react";
 import { API_URL } from "../../constants";
 import { QuestionCardList } from "../../components/QuestionCardList";
 import { Loader } from "../../components/Loader";
 import { useFetch } from "../../hooks/useFetch";
 import cls from "./HomePage.module.css";
 import { SearchInput } from "../../components/SearchInput";
+import { Button } from "../../components/Button";
+
+const DEFAULT_PER_PAGE = 10;
 
 export const HomePage = () => {
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState({});
+  const [searchParams, setSearchParams] = useState(
+    `?_page=1&_per_page=${DEFAULT_PER_PAGE}`
+  );
   const [searchValue, setSearchValue] = useState("");
   const [sortSelectValue, setSortSelectValue] = useState("");
+
+  const controlsContainerRef = useRef();
+
+  const getActivePageNumber = () =>
+    questions.next === null ? questions.last : questions.next - 1;
 
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`);
@@ -20,40 +31,51 @@ export const HomePage = () => {
   });
 
   const cards = useMemo(() => {
-    return questions.filter((d) =>
-      d.question.toLowerCase().includes(searchValue.trim().toLowerCase())
-    );
+    if (questions?.data) {
+      if (searchValue.trim()) {
+        return questions.data.filter((d) =>
+          d.question.toLowerCase().includes(searchValue.trim().toLowerCase())
+        );
+      } else {
+        return questions.data;
+      }
+    }
+    return [];
   }, [questions, searchValue]);
 
-  // const getQuestions = async () => {
-  //   try {
-  //     setIsLoading(true);
-  //     await delayFn();
-  //     const response = await fetch(`${API_URL}/react`);
-  //     const questions = await response.json();
+  const pagination = useMemo(() => {
+    const totalCardsCount = questions?.pages || 0;
 
-  //     setQuestions(questions);
-  //   } catch (error) {
-  //     console.error(error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
+    return Array(totalCardsCount)
+      .fill(0)
+      .map((_, i) => i + 1);
+  }, [questions]);
 
   useEffect(() => {
-    getQuestions(`react?${sortSelectValue}`);
-  }, [sortSelectValue]);
+    getQuestions(`react${searchParams}`);
+  }, [searchParams]);
 
   const onSearchChangeHandler = (e) => {
     setSearchValue(e.target.value);
   };
   const onSortSelectChangeHandler = (e) => {
     setSortSelectValue(e.target.value);
+
+    setSearchParams(`?_page=1&_per_page=${DEFAULT_PER_PAGE}&${e.target.value}`);
+  };
+
+  const paginationHandler = (e) => {
+    if (e.target.tagName === "BUTTON") {
+      setSearchParams(
+        `?_page=${e.target.textContent}&_per_page=${DEFAULT_PER_PAGE}&${sortSelectValue}`
+      );
+      controlsContainerRef.current.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   return (
     <>
-      <div className={cls.controlsContainer}>
+      <div className={cls.controlsContainer} ref={controlsContainerRef}>
         <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
 
         <select
@@ -71,9 +93,22 @@ export const HomePage = () => {
 
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
-      {cards.length === 0 && <p className={cls.noCardsInfo}>No cards...</p>}
 
       <QuestionCardList cards={cards} />
+
+      {cards.length === 0 ? (
+        <p className={cls.noCardsInfo}>No cards...</p>
+      ) : (
+        <div className={cls.paginationContainer} onClick={paginationHandler}>
+          {pagination.map((value) => {
+            return (
+              <Button key={value} isActive={value === getActivePageNumber()}>
+                {value}
+              </Button>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
