7e668f460dcadec3d1ec018741beb27cd75c9d7c
Misha <mishaelb7@gmail.com>
2025-07-19 15:08:33 +0300

    add question page lazy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { HomePage } from "./pages/HomePage";
 import { QuestionCard } from "./components/QuestionCard";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { QuestionPage } from "./pages/QuestionPage";
-import { AddQuestionPage } from "./pages/AddQuestionPage";
+import { AddQuestionPage, AddQuestionPageLazy } from "./pages/AddQuestionPage";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/forbidden" element={<div>forbidden (^_^)</div>} />
-          <Route path="/addquestion" element={<AddQuestionPage />} />
+          <Route path="/addquestion" element={<AddQuestionPageLazy />} />
           <Route path="/question/:id" element={<QuestionPage />} />
 
           <Route path="*" element={<NotFoundPage />} />
--- src/pages/AddQuestionPage/AddQuestionPage.jsx
+++ src/pages/AddQuestionPage/AddQuestionPage.jsx
@@ -41,7 +41,7 @@ const createCardAction = async (_prevState, formData) => {
   }
 };
 
-export const AddQuestionPage = () => {
+const AddQuestionPage = () => {
   const [formState, formAction, isPending] = useActionState(createCardAction, {
     clearForm: true,
   });
@@ -123,3 +123,5 @@ export const AddQuestionPage = () => {
     </>
   );
 };
+
+export default AddQuestionPage;
