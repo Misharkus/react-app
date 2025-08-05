b8c302b330365e973108fe98e456f7d9eb8105f2
Михайло <mykhailo@MacBook-Air-Mihajlo.local>
2025-08-05 12:00:16 +0300

    Update Not Found Page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,23 @@
 import cls from "./NotFoundPage.module.css";
+import logo from "../../assets/404Error-with-a-cute-animal.svg";
+import { useNavigate } from "react-router-dom";
 
 export const NotFoundPage = () => {
-  return <h2 className={cls.title}>Not Found Page</h2>;
+  const navigate = useNavigate();
+  return (
+    <div className={cls.notFoundContainer}>
+      {/* <div className={cls.notFoundContent}> */}
+      <img src={logo} alt="404 Not Found" className={cls.notFoundImage} />
+      <h1 className={cls.notFoundTitle}>Ой! Сторінку не знайдено</h1>
+      <p className={cls.notFoundDescription}>
+        Можливо, вона була видалена або переміщена. Спробуйте повернутися на
+        головну.
+      </p>
+      <button className={cls.notFoundButton} onClick={() => navigate("/")}>
+        На головну
+      </button>
+    </div>
+    // </div>
+  );
 };
+<a href="https://storyset.com/web">Web illustrations by Storyset</a>;
